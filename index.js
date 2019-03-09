4c2aeaf4321ca47b59fe62d4b6639cb9186b90c6
Angelo Spampinato <spampinatodesign@gmail.com>
2019-03-09 14:07:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+// function companyName() {
+//   return 'Scuber';
+// }
+
+const companyName = "Scuber";
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
